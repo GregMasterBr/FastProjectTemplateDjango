379af903d838fc615d85b61a3501bca787238b65
unknown <gregmasterbr@gmail.com>
2023-03-02 17:25:30 -0300

    Adicionando o Django Messages

--- project_name/core/views.py
+++ project_name/core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from django.contrib.messages import constants
 # Create your views here.
 
 def home(request):
+    messages.add_message(request, constants.SUCCESS, 'Página carregada com sucesso')
     return render(request,'index.html')
